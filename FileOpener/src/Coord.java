01e5a2a1cd2a12b244cadf114ebe02adc39e47e1
Simon Andersson <andsimo@student.chalmers.se>
2015-01-27 19:02:23 +0100

    FileOpener läser och lagrar numer relevant data (Latitud och Longitude). Koordinaterna lagras i Coord Klassen som i dagsläget även innehåller filnamnet de härstammar ifrån. Loggar med koordinater (0, 0) sorteras bort. Vad göra med dessa??

---
+++ FileOpener/src/Coord.java
@@ -0,0 +1,47 @@
+/*
+ * Vårt object för att lagra coordinater i. Lagrar longitud och latitude som doubles samt filnamnet ifall detta skulle behövas.
+ * Enkel att bygga ut.
+ */
+public class Coord {
+
+	private double longitude;
+	private double latitude;
+	private String file;
+	
+	public Coord(){}
+	
+	public Coord(String file){
+		super();
+		this.file = file;
+	}
+	
+	public Coord(double longitude, double latitude){
+		super();
+		this.longitude = longitude;
+		this.latitude = latitude;
+		
+	}
+	
+	
+	public void setLong(double longitude){
+		this.longitude = longitude;
+	}
+	
+	public void setLat(double latitude){
+		this.latitude = latitude;
+	}
+	
+	public String getFile(){
+		return file;
+	}
+	
+	public double getLong(){
+		return longitude;
+	}
+	
+	public double getLat(){
+		return latitude;
+	}
+
+
+}
--- FileOpener/src/FileOpener.java
+++ FileOpener/src/FileOpener.java
@@ -5,6 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class FileOpener {
@@ -13,10 +17,12 @@ public class FileOpener {
 	private BufferedReader in;
 	private File file;
 	private File[] listOfFiles;
-										//Lägga till något sätt att lagra texten... eller rättare sagt, den relevanta delen av texten.
+	private ArrayList<Coord> sensors;	
+	
 	
 	public FileOpener(File filePath){
-		
+		sensors = new ArrayList<Coord>();
+		//i = 0;
 		file = filePath;
 		listOfFiles = file.listFiles();
 		
@@ -31,39 +37,89 @@ public class FileOpener {
 	public void OpenFiles(){					
 		for(File file: listOfFiles){
 			
+			
+			
 			if(file.toString().toLowerCase().endsWith(".txt") || file.toString().toLowerCase().endsWith(".log")){
-			System.out.println(file.toString());
+			//System.out.println(file.toString());
 			
 			
 			
 			try {
 				in = new BufferedReader(new FileReader(file));
+				Coord coord = new Coord(file.toString());
 				
 				StringBuilder sb = new StringBuilder();
 				String line = in.readLine();
 		
 				
 				while(line != null){
-					sb.append(line);
-					sb.append(System.lineSeparator());
+					
+					if(line.contains("Longitude") ){
+						
+						
+						sb.append(line);
+						sb.append(System.lineSeparator());
+						
+						String[] sla = sb.toString().split(":");
+						coord.setLong(Double.parseDouble(sla[1]));
+						
+						sb = new StringBuilder();
+	
+					
+					}
+					else if(line.contains("Latitude") ){
+						
+						
+						sb.append(line);
+						sb.append(System.lineSeparator());
+						
+						String[] sla = sb.toString().split(":");
+						coord.setLat(Double.parseDouble(sla[1]));
+						
+						sb = new StringBuilder();
+						
+					}
+					
 					line = in.readLine();
+					
 				}
 				
-				String sl = sb.toString();
-				System.out.println(sl);
 				
-			
+				if(coord.getLat() == 0 && coord.getLong() == 0){
+					coord = null;
+				}
+				else{
+				sensors.add(coord);
+				}
 			
 			} catch (IOException e) {
 				System.out.println("File " + file + " not found");
 				System.out.println("or something else...");
 				e.printStackTrace();
-			} 
+			}
+			
+			
 			
 		}
+			
 		}
 		
+		sendToExcel();
 	
+	}
+	
+	
+	public void sendToExcel(){
+		
+		
+		
+		/*
+		 * Avkommentera ifall utskrivning önskas!
+		 * 
+		    for(Coord coord: sensors){
+			System.out.println(coord.getFile());
+			System.out.println("Long: " + coord.getLong() + "\t Lat: " + coord.getLat() + "\n");
+		}*/
 	}
 	
 	
