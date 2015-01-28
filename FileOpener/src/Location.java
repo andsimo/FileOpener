c1a24711c4b1385fbf9932b94de16a2b1eb41e02
Simon <xsiand31@gmail.com>
2015-01-28 14:33:54 +0100

    Tog bort, testfilen. Lagt till första variabler och konstruktorer för Location.

--- FileOpener/src/Location.java
+++ FileOpener/src/Location.java
@@ -2,5 +2,20 @@
 public class Location {
 
 	private Coord coord;
+	private int numSensors;
+	private String city;
+	private String country;
+	
+	
+	
+	public Location(){
+		
+	}
+	
+	public Location(Coord coord, String city, String country){
+		this.coord = coord;
+		this.city = city;
+		this.country = country;
+	}
 	
 }
