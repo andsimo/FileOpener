efce5166d33738846f9384fb34b018b094b85b9d
Simon <xsiand31@gmail.com>
2015-01-28 14:55:05 +0100

    Location, start.

--- FileOpener/src/Location.java
+++ FileOpener/src/Location.java
@@ -12,10 +12,11 @@ public class Location {
 		
 	}
 	
-	public Location(Coord coord, String city, String country){
+	public Location(Coord coord, String city, String country, int numSensors){
 		this.coord = coord;
 		this.city = city;
 		this.country = country;
+		this.numSensors = numSensors;
 	}
 	
 }
