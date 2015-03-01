8792817b8a4d0e31107ecd9ae642d5e0f2b1adaf
Emil <Emil@Emil-Dator>
2015-03-01 20:08:17 +0100

    Tagit bort imports

--- FileOpener/src/FileOpener.java
+++ FileOpener/src/FileOpener.java
@@ -2,9 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
